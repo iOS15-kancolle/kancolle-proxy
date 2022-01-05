e850fb0e8996b8e1d7df1c82196560cec6b4b4bc
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-01-05 15:48:38 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -27,19 +27,109 @@ server.on('connect', function (req, socket) {
   });
 });
 
-proxy.on('proxyRes', (proxyRes: IncomingMessage): void => {
-    switch(proxyRes.url){
-        case "":
+proxy.on('proxyReq', (proxyReq: http.ClientRequest): void => {
+      switch(proxyReq.url){
+        //入渠開始
+        /*
+        POST /kcsapi/api_req_nyukyo/start
+          api_token: token
+          api_verno: 1
+          api_highspeed: 0,1 高速修復
+          api_ndock_id: 入渠ドックID #_level1.dockIdTmp + 1
+          api_ship_id: 入渠する艦のapi_id? #_level1.nyukyo_shipid[_level1.dockIdTmp]
+        */
+        case "/kcsapi/api_req_nyukyo/start":
         
         break;
-        
-        case "":
+        //入渠高速化
+        /*
+        POST /kcsapi/api_req_nyukyo/speedchange
+          api_token: token
+          api_verno: 1
+          api_ndock_id: 入渠ドックID #_level1.dockIdTmp + 1
+        */
+        case "/kcsapi/api_req_nyukyo/speedchange":
         
         break;
+        //建造開始
+        /*
+        POST /kcsapi/api_req_kousyou/createship
+          api_token: token
+          api_verno: 1
+          api_large_flag: 建造モード (大型建造フラグ？ 燃料？が1000超えると立つ 大型艦建造モードの場合開発資材を10個消費する)
+          api_highspeed: 高速建造 #_level1.k5use
+          api_kdock_id: 工廠ドックID
+          api_item1: 燃料？
+          api_item2: 弾薬？
+          api_item3: 鉄？
+          api_item4: ボーキ？
+          api_item5: 不明 1固定 #_level1.m7
+        */
+        case "/kcsapi/api_req_kousyou/createship":
         
-        case "":
+        break;
+        //建造高速化
+        /*
+        POST /kcsapi/api_req_kousyou/createship_speedchange
+          api_token: token
+          api_verno: 1
+          api_highspeed: 1
+          api_kdock_id: ドックID
+        */
+        case "/kcsapi/api_req_kousyou/createship_speedchange":
         
-        break;       
+        break;
+        //遠征開始
+        /*
+        POST /kcsapi/api_req_mission/start
+          api_token: token
+          api_verno: 1
+          api_mission_id: 遠征ID？ #_level1.SYS_mission[_level1.mid].api_id
+          api_deck_id: デッキID #_level1.deckid + 1
+        */
+        case "/kcsapi/api_req_mission/start":
+        
+        break;
+    } 
+})
+
+proxy.on('proxyRes', (proxyRes: http.IncomingMessage): void => {
+      switch(proxyRes.url){
+        //入渠開始
+        /*
+        POST /kcsapi/api_req_nyukyo/start
+        */
+        case "/kcsapi/api_req_nyukyo/start":
+        
+        break;
+        //入渠高速化
+        /*
+        POST /kcsapi/api_req_nyukyo/speedchange
+        */
+        case "/kcsapi/api_req_nyukyo/speedchange":
+        
+        break;
+        //建造開始
+        /*
+        POST /kcsapi/api_req_kousyou/createship
+        */
+        case "/kcsapi/api_req_kousyou/createship":
+        
+        break;
+        //建造高速化
+        /*
+        POST /kcsapi/api_req_kousyou/createship_speedchange
+        */
+        case "/kcsapi/api_req_kousyou/createship_speedchange":
+        
+        break;
+        //遠征開始
+        /*
+        POST /kcsapi/api_req_mission/start
+        */
+        case "/kcsapi/api_req_mission/start":
+        
+        break;
     }
 })
 
