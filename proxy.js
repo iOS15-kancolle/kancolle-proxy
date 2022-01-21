cd28057f10f03a56af18dab1f630ff49fd05dceb
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-01-21 10:39:21 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -48,7 +48,7 @@ proxy.on('proxyRes', (proxyRes, req, res, options) => {
 	  console.log('Receiving reverse proxy response for:' + req.url);
 	  if(!req.url.match(/kcsapi/)) return;
           //console.log(data);
-	  console.log(parsedata(resDataChunks,res));
+	  console.log(parsedata(resDataChunks,res.headers));
           dataparse(req.url,data,true);
       });
 });
