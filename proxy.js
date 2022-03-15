5d80bb718227992d9c5dfabc3531bf74145cab42
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-03-15 23:36:00 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -40,7 +40,7 @@ var server = http.createServer(function (req, res) {
     return;
   }
   */
-  proxy.web(req, res, { target: target, secure: true, enable: { xforward: true } } , function(e) { console.log(e) });
+  proxy.web(req, res, { target: target, ssl: true, secure: false, enable: { xforward: true } } , function(e) { console.log(e) });
 }).listen(process.env.PORT || 7778);
 
 /*
@@ -56,14 +56,14 @@ server.on('connect', function (req, socket, bodyhead) {
   var hostDomain = hostPort[0];
   var port = parseInt(hostPort[1]);
   console.log("Proxying HTTPS request for:", hostDomain, port);	
-	
+/*
   if(req.url.match(/www.dmm.com/) ){//&& req.url.match(/app/) && req.url.match(/854854/) ){ //&& req.url.match(/httpstohttp/)){
     socket.write("HTTP/1.1 301 Moved Permanently\r\nLocation: http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/\r\n");
     socket.end();
     console.log("redirected");
     return;
   }
-  
+  */
   var serverUrl = url.parse('https://' + req.url);
 
   var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function () {
