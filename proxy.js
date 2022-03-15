33911d1594a03fa09e92ae2939d7c1685047368a
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-03-15 23:03:16 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -38,18 +38,13 @@ var server = http.createServer(function (req, res) {
     return;
   }
   */
-  proxy.web(req, res, { target: target,
-  ssl: {
-    key: fs.readFileSync('key.pem', 'utf8'),
-    cert: fs.readFileSync('cert.pem', 'utf8')
-  },
-  /*secure: true,*/ enable: { xforward: true } } , function(e) { console.log(e) });
+  proxy.web(req, res, { target: target,secure: false, enable: { xforward: true } } , function(e) { console.log(e) });
 }).listen(process.env.PORT || 7778);
 
 server.on('connect', function (req, socket) {
   //console.log('Receiving reverse proxy request for:' + req.url);
 
-  var serverUrl = url.parse('http://' + req.url);
+  var serverUrl = url.parse('https://' + req.url);
 
   var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function () {
     socket.write('HTTP/1.1 200 Connection Established\r\n' +
@@ -62,14 +57,13 @@ server.on('connect', function (req, socket) {
 
 
 server.addListener('connect', function (req, socket, bodyhead) {
+	return;
   var hostPort = getHostPortFromString(req.url, 443);
   var hostDomain = hostPort[0];
   var port = parseInt(hostPort[1]);
-  if(hostDomain!="www.dmm.com")return;
+  //if(hostDomain!="www.dmm.com")return;
   console.log("Proxying HTTPS request for:", hostDomain, port);
-　console.log('Receiving reverse proxy request for:' + req.url);
-  console.log(socket);
-  console.log("---------------------------------------------------")
+	/*
   if(req.url.match(/www.dmm.com/) && req.url.match(/app/) && req.url.match(/854854/) ){ //&& req.url.match(/httpstohttp/)){
     socket.writeHead(301, {
     'Location': 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/'
@@ -78,6 +72,7 @@ server.addListener('connect', function (req, socket, bodyhead) {
     console.log("redirected");
     return;
   }
+  */
 });
 
 proxy.on('proxyReq', (proxyReq, req, res, options) => { 
